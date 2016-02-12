d177573eca9dd1bbdcaf590ca24fa264c7d7dd21
Jake Waksbaum <jake.waksbaum@gmail.com>
2016-02-12 10:50:19 -0500

    Remove gen_pic to make a library only branch

--- gen_pic.hs
+++
@@ -1,8 +0,0 @@
-import Pbm
-
-main :: IO ()
-main = writePbm "pic.pbm" pic
-  where f1 (Pair x y) = round $ exp( (fromIntegral x) * (fromIntegral y))
-        f2 (Pair x y) = x*y
-        f3 (Pair x y) = round $ (sin (fromIntegral x) + sin (fromIntegral y) + 1) * 75
-        pic = mathPic (Triple f1 f2 f3) (Pair 500 500)
