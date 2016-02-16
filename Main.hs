c4b83805f60255d32c1c379834a3e53d074020cf
Jake Waksbaum <jake.waksbaum@gmail.com>
2016-02-15 20:58:21 -0500

    Refactor functions

--- Main.hs
+++ Main.hs
@@ -2,7 +2,9 @@ import Pbm
 
 main :: IO ()
 main = writePbm "pic.pbm" pic
-  where f1 (Pair x y) = round $ exp( (fromIntegral x) * (fromIntegral y))
+  where f1 p = round (exp $ xgen * ygen :: Float)
+          where Pair xgen ygen = fmap fromIntegral p
         f2 (Pair x y) = x*y
-        f3 (Pair x y) = round $ (sin (fromIntegral x) + sin (fromIntegral y) + 1) * 75
+        f3 p = round ((sin xgen :: Float) + sin ygen + 1) * 75
+           where Pair xgen ygen = fmap fromIntegral p
         pic = mathPic (Triple f1 f2 f3) (Pair 500 500)
