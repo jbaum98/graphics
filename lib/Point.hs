bb0cb65c7420209a0e44776aed8e6e7b19df5561
Jake Waksbaum <jake.waksbaum@gmail.com>
2016-02-17 14:47:43 -0500

    Add translate function

--- lib/Point.hs
+++ lib/Point.hs
@@ -1,6 +1,13 @@
-module Point (Coord, Point, Pair(..)) where
+module Point (
+  Coord, Point, Pair(..),
+  translate
+             ) where
 
 import Pair
+import Control.Applicative
 
 type Coord = Integer
 type Point = Pair Coord
+
+translate :: Point -> Point -> Point
+translate by p = (+) <$> by <*> p
