342268d6c426852421f69a4ce806f2b8cdb1bb25
Jake Waksbaum <jake.waksbaum@gmail.com>
2016-02-17 16:44:13 -0500

    Draw a list of lines

--- Main.hs
+++ Main.hs
@@ -1,5 +1,16 @@
 import Pbm
+import Control.Applicative ((<$>), (<*>))
 
 main :: IO ()
 main = writePbm "pic.pbm" pic
-  where pic = drawLine (Pair 0 0) (Pair 100 70) $ blankPic (Pair 100 100)
+  where maxPoint = Pair 100 100
+        origin = (round . (/2) . fromIntegral) <$> maxPoint
+        lineList = [
+          Pair (Pair 0 0) (Pair 100 70),
+          Pair (Pair 0 (-10)) (Pair 100 70)
+          ]
+        pic = drawLines origin lineList $ blankPic maxPoint
+
+drawLines :: Point -> [Pair Point] -> Picture -> Picture
+drawLines origin points = foldl (.) id $ drawAllLines points
+   where drawAllLines = map $ setColor black . fmap (setOrigin origin) . uncurryPair line
