726a0843cd90d5bb3bb565976216a6abeeb3fb34
Jake Waksbaum <jake.waksbaum@gmail.com>
2016-02-19 00:43:53 -0500

    Delete dir-locals

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((haskell-mode
-  (flycheck-ghc-search-path "lib")
-  (flycheck-disabled-checkers haskell-stack-ghc)
-  (global-flycheck-mode . t)))
