1a7a801cb5dcbf08c877c2792f0c6fb018dada4e
Jake Waksbaum <jake.waksbaum@gmail.com>
2016-02-25 10:23:44 -0500

    Fix benchmarks to be dummies

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -1,24 +1,18 @@
 import Criterion.Main
 import Pair
-import GenerateFile (pic)
 import Pbm (writePbm)
 import Picture (blankPic)
 
 main :: IO ()
 main = defaultMain [
-         bgroup "rendering lines" [
-             bench "100x100"   $ whnf pic (Pair 100 100)
-           , bench "500x500"   $ whnf pic (Pair 500 500)
-           , bench "1000x1000" $ whnf pic (Pair 1000 1000)
-             ]
-         , bgroup "create blank picture" [
-              bench "100x100"   $ whnf blankPic (Pair 100 100)
-            , bench "500x500"   $ whnf blankPic (Pair 500 500)
-            , bench "1000x1000" $ whnf blankPic (Pair 1000 1000)
-             ]
-         , bgroup "writing" [
-               bench "100x100"   . nfIO . writePbm "bench.ppm" . blankPic $ Pair 100 100
-             , bench "500x500"   . nfIO . writePbm "bench.ppm" . blankPic $ Pair 500 500
-             , bench "1000x1000" . nfIO . writePbm "bench.ppm" . blankPic $ Pair 1000 1000
-             ]
-          ]
+  bgroup "create blank picture" [
+      bench "100x100"   $ nf blankPic $ Pair 100 100
+      , bench "500x500"   $ nf blankPic $ Pair 500 500
+      , bench "1000x1000" $ nf blankPic $ Pair 1000 1000
+      ]
+  , bgroup "writing" [
+      bench "100x100"   . nfIO . writePbm "bench.ppm" . blankPic $ Pair 100 100
+      , bench "500x500"   . nfIO . writePbm "bench.ppm" . blankPic $ Pair 500 500
+      , bench "1000x1000" . nfIO . writePbm "bench.ppm" . blankPic $ Pair 1000 1000
+      ]
+  ]
