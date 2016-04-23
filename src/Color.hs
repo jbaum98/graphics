e656ed504d1b79cb533cfe8c077e64ddc4325904
Jake Waksbaum <jake.waksbaum@gmail.com>
2016-04-23 16:24:25 -0400

    Use bulk type signatures in Color

--- src/Color.hs
+++ src/Color.hs
@@ -28,13 +28,9 @@ type Color = Triple ColorVal
 maxColor :: ColorVal
 maxColor = 255
 
-getRed   :: Color -> ColorVal
+getRed, getGreen, getBlue :: Color -> ColorVal
 getRed   (Triple r _ _) = r
-
-getGreen :: Color -> ColorVal
 getGreen (Triple _ g _) = g
-
-getBlue  :: Color -> ColorVal
 getBlue  (Triple _ _ b) = b
 
 -- |Convert a color value from a system with a different maximum color value
@@ -51,35 +47,16 @@ relative oldMax oldVal = round $ fromIntegral maxColor * oldVal / oldMax
 rel255 :: RealFrac a => a -> ColorVal
 rel255 = relative 255
 
-black :: Color
+black, white, red, orange, yellow, green, blue, indigo, violet, pink, turqouise :: Color
+
 black = Triple 0 0 0
-
-white :: Color
 white = Triple maxColor maxColor maxColor
-
-red :: Color
 red = Triple maxColor 0 0
-
-orange :: Color
 orange = rel255 <$> Triple 255 144 0
-
-yellow :: Color
 yellow = Triple maxColor maxColor 0
-
-green :: Color
 green = Triple 0 maxColor 0
-
-blue :: Color
 blue = Triple 0 0 maxColor
-
-indigo :: Color
 indigo = rel255 <$> Triple 75 0 130
-
-violet :: Color
 violet = rel255 <$> Triple 159 0 255
-
-pink :: Color
 pink = rel255 <$> Triple 255 192 203
-
-turqouise :: Color
 turqouise = rel255 <$> Triple 64 224 208
